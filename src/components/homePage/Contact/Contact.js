1925d0fce10dddb7fdcfdf86ef1347b2b4f783e0
Damian <damianszmurlowork@gmail.com>
2020-03-03 14:00:59 +0100

    fixed bug with email

--- src/components/homePage/Contact/Contact.js
+++ src/components/homePage/Contact/Contact.js
@@ -14,50 +14,25 @@ class Contact extends React.Component{
 
 
                         <form
-    name="contact-da"
-    method="post"
-    action="/success"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-  >
-    <input type="hidden" name="bot-field" />
-    <div>
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" />
-    </div>
-    <div>
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" />
-    </div>
-    <div>
-      <label htmlFor="message">Message</label>
-      <textarea name="message" id="message" rows="6" required />
-    </div>
-    <div>
-      <input type="submit" value="Drop a line" />
-      <input type="reset" value="Eraser" />
-    </div>
-  </form>
+                            name="contact-da"
+                            method="post"
+                            action="/success"
+                            data-netlify="true"
+                            data-netlify-honeypot="bot-field"
+                        >
+                            <input type="hidden" name="bot-field" />
+                            <label htmlFor="name"></label>
+                            <input placeholder="First name" className="contact__firstname"  type="text" name="name" id="name" />
+                           
+                            <label htmlFor="email"></label>
+                            <input placeholder="Email" className="contact__email" type="text" name="email" id="email" />
+                          
+                            <label htmlFor="message"></label>
+                            <textarea className="contact__textarea" name="message" id="message" rows="6" required />
+                
+                            <button className="contact__btn g-btn" type="submit" value="Send"><p>Send</p></button>
 
-                        {/* <form name="contact-1" method="POST" data-netlify="true">          
-
-                            <label htmlFor="email">
-                                <input id="email" placeholder="Email" className="contact__email" type="email" name="email"/>
-                            </label>
-
-                            <label htmlFor="firstname">
-                                <input id="firstname" placeholder="First name" className="contact__firstname" type="text" name="name"/>
-                            </label>
-
-                            <label htmlFor="content" htmlFor="lastname">
-                                <textarea id="content" type="text" className="contact__textarea"/>
-                            </label>
-
-                            <div data-netlify-recaptcha="true"></div>
-
-                            <button type="submit" className="contact__btn g-btn"><p>Send</p></button>
-
-                        </form> */}
+                        </form>
 
                     </div>
 
