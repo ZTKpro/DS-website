d7633a08fdb8068f75ee4cf676a482195051dff6
Damian <damianszmurlowork@gmail.com>
2020-03-03 13:00:14 +0100

    dcv

--- src/components/homePage/Contact/Contact.js
+++ src/components/homePage/Contact/Contact.js
@@ -12,7 +12,29 @@ class Contact extends React.Component{
 
                         <h2>Contact us</h2>
 
-                        <form name="contact-1" method="POST" data-netlify="true">          
+                        
+                        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+                        {/* <form name="contact-1" method="POST" data-netlify="true">          
 
                             <label htmlFor="email">
                                 <input id="email" placeholder="Email" className="contact__email" type="email" name="email"/>
@@ -30,7 +52,7 @@ class Contact extends React.Component{
 
                             <button type="submit" className="contact__btn g-btn"><p>Send</p></button>
 
-                        </form>
+                        </form> */}
 
                     </div>
 
