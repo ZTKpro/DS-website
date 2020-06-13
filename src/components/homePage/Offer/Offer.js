c67c4e87d75c30ed2f6dccc68f6a36730c445cce
ZTKpro <damianszmurlowork@gmail.com>
2020-06-13 10:23:32 +0200

    refresh

--- src/components/homePage/Offer/Offer.js
+++ src/components/homePage/Offer/Offer.js
@@ -1,175 +1,85 @@
 import React from "react"
-import './Offer.scss'
+import "./Offer.scss"
 
+import website from "./img/website.png"
+import CMS from "./img/CMS.png"
+import rwd from "./img/rwd.png"
+import hosting from "./img/hosting.png"
+import email from "./img/email.png"
+import ssl from "./img/SSL.png"
+import wrench from "./img/wrench.png"
+import designer from "./img/designer.png"
+import customer from "./img/customer.png"
+import fb from "./img/fb.png"
+import card from "./img/card.png"
 
-import website from './img/website.png'
-import CMS from './img/CMS.png'
-import rwd from './img/rwd.png'
-import hosting from './img/hosting.png'
-import email from './img/email.png'
-import ssl from './img/SSL.png'
-import wrench from './img/wrench.png'
-import designer from './img/designer.png'
-import customer from './img/customer.png'
-import fb from './img/fb.png'
-import card from './img/card.png'
+class Offer extends React.Component {
+  render() {
+    return (
+      <div className="offer fadeIn">
+        <div className="g-container offer__containers">
+          <div className="offer__item">
+            <h4>Stawka godzinowa</h4>
+            <p className="offer__price">50zł/h</p>
+            <p className="offer__description">
+              Potrzebujesz przyspieszyć budowę aplikacji ale nie chcesz
+              zatrudniać programisty na stałe ?
+            </p>
+            <ul className="offer__ul">
+              <li className="offer__li">
+                <p>Czysty kod</p>
+              </li>
+              <li className="offer__li">
+                <p>Profesjonalne podejście</p>
+              </li>
+            </ul>
+          </div>
 
+          <div className="offer__item">
+            <h4>B2B</h4>
+            <p className="offer__price">6000 zł/netto</p>
+            <p className="offer__description">
+              Brakuje ci rąk w projekcie i poszukujesz kogoś do stałej
+              współpracy?
+            </p>
+            <ul className="offer__ul">
+              <li className="offer__li">
+                <p>Czysty kod</p>
+              </li>
+              <li className="offer__li">
+                <p>Profesjonalne podejście</p>
+              </li>
+              <li className="offer__li">
+                <p>Pełne zaangażowanie</p>
+              </li>
+            </ul>
+          </div>
 
-class Offer extends React.Component{
-
-    render() {
-        return(
-            <div className="offer fadeIn">
-
-                    <div className="g-container offer__containers">
-
-
-                        <div className="offer__item">
-                            <h4>Basic</h4>
-                            <p className="offer__price">€ 300</p> 
-                            <p className="offer__description">The basic website package that works best when you 
-                                need a simple and elegant business card of your
-                                 company.</p>
-                            <ul className="offer__ul">
-                                <li className="offer__li">
-                                    <img src={website} alt="shield icon"></img>
-                                    <p>Professional website</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={CMS} alt="cms icon"></img>
-                                    <p>System CMS</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={rwd} alt="computer icon"></img>
-                                    <p>RWD</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={hosting} alt="piggybank"></img>
-                                    <p>Free hosting 1 years</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={ssl} alt="padlock"></img>
-                                    <p>SSL certificate</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={email} alt="mailbox icon"></img>
-                                    <p>Mailbox</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={customer} alt="wrench icon"></img>
-                                    <p>Support for 3 months</p>
-                                </li>    
-                            </ul>   
-                        </div>
-                        
-                    
-                        <div className="offer__item">
-                            <h4>Business</h4>
-                            <p className="offer__price">€ 700</p> 
-                            <p className="offer__description">You need a professional website
-                             that will bring you profits.This package is  for you</p>
-                            <ul className="offer__ul">
-                                <li className="offer__li">
-                                    <img src={website} alt="shield icon"></img>
-                                    <p>Professional website</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={CMS} alt="cms icon"></img>
-                                    <p>System CMS</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={rwd} alt="computer icon"></img>
-                                    <p>RWD</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={hosting} alt="piggybank"></img>
-                                    <p>Free hosting 1 years</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={ssl} alt="padlock"></img>
-                                    <p>SSL certificate</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={email} alt="mailbox icon"></img>
-                                    <p>Mailbox</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={wrench} alt="wrench icon"></img>
-                                    <p>Support for 3 months</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={designer} alt="designer icon"></img>
-                                    <p>Created by our best graphic designer.</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={customer} alt="wrench icon"></img>
-                                    <p>In 100% tailored to customer</p>
-                                </li>    
-                            </ul>   
-                        </div>
-
-
-                        <div className="offer__item">
-                            <h4>Master</h4>
-                            <p className="offer__price">€ 1400</p> 
-                            <p className="offer__description">Do you need comprehensive help 
-                            in promoting your business on the Internet? 
-                            This package is for you</p>
-                            <ul className="offer__ul">
-                                <li className="offer__li">
-                                    <img src={website} alt="shield icon"></img>
-                                    <p>Professional website</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={CMS} alt="cms icon"></img>
-                                    <p>System CMS</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={rwd} alt="computer icon"></img>
-                                    <p>RWD</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={hosting} alt="piggybank"></img>
-                                    <p>Free hosting 1 years</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={ssl} alt="padlock"></img>
-                                    <p>SSL certificate</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={email} alt="mailbox icon"></img>
-                                    <p>Mailbox</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={wrench} alt="wrench icon"></img>
-                                    <p>Support for 3 months</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={designer} alt="designer icon"></img>
-                                    <p>Created by our best graphic designer.</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={customer} alt="wrench icon"></img>
-                                    <p>In 100% tailored to customer</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={fb} alt="facebook icon"></img>
-                                    <p>Boosting fanpage on facebook</p>
-                                </li>    
-                                <li className="offer__li">
-                                    <img src={card} alt="card icon"></img>
-                                    <p>Professional  
-Logo , Banner ad and business card </p>
-                                </li>    
-                            </ul>   
-                        </div>
-
-                    </div>
-
-            </div>
-        )
-    }
-
+          <div className="offer__item">
+            <h4>Projekt</h4>
+            <p className="offer__price">----</p>
+            <p className="offer__description">
+              Masz pomysł na aplikacje ale braku ci rąk aby to zrealizować?
+            </p>
+            <ul className="offer__ul">
+              <li className="offer__li">
+                <p>Czysty kod</p>
+              </li>
+              <li className="offer__li">
+                <p>Profesjonalne podejście</p>
+              </li>
+              <li className="offer__li">
+                <p>Pełne zaangażowanie</p>
+              </li>
+              <li className="offer__li">
+                <p>Różne formy rozliczenia</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Offer;
+export default Offer
